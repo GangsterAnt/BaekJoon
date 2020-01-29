0d99b1e80c675900cd3539a53ef17b900591b3e6
GangsterAnt <49489178+GangsterAnt@users.noreply.github.com>
2020-01-30 03:27:31 +0900

    Update 2293 동전1.cpp

--- Easy Dynamic programming/2293 동전1.cpp
+++ Easy Dynamic programming/2293 동전1.cpp
@@ -30,7 +30,12 @@ int iterative(int coin_index, int cost)
 	{
 		/*for (int i = 0; i <= goal; ++i)
 			cout << k[i] << " ";
-		cout << '\n';*/
+		점화식 .. iterative(i , cost) = iterative( i-1, cost) + iterative( i, cost - coin[i])
+						 i 제외한 coin으로만 구성된것  coin[i]를 포함한 cost-coin[i]를 구성한것 
+					우항은 다시 점화식으로 쪼개지며 .
+	순서대로 iterative(i ,cost - coin[i]) -> coin[i]를 1번 쓴것.
+	iterative( i, cost - coin[i] - coin[i]) -> coin[i]를 2번 쓴것...... 을 모두 커버한다
+ 		cout << '\n';*/
 		for (int j = 1; j <= cost; ++j)
 			if (j - n[i] >= 0)
 				k[j] += k[j - n[i]];
