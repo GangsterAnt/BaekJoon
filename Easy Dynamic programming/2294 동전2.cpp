5530a44453c75a577d1662702b0dd2d77326eae4
GangsterAnt <49489178+GangsterAnt@users.noreply.github.com>
2020-01-30 03:20:57 +0900

    Update 2294 동전2.cpp

--- Easy Dynamic programming/2294 동전2.cpp
+++ Easy Dynamic programming/2294 동전2.cpp
@@ -10,14 +10,14 @@ int coin_num, goal;
 
 int main()
 {
-	k[0] = 0;
+	k[0] = 0;	//기저..0 원을 만들기 위한 동전은? 0개
 
 
 
 	cin >> coin_num >> goal;
 
 	for (int i = 1; i <= goal; ++i)
-		k[i] = 10000;
+		k[i] = 10000;	//최소값을 구하는 문제이기때문에 큰 임의의 값 할당.
 
 	for (int i = 1; i <= coin_num; ++i)
 		cin >> coin[i];
@@ -38,6 +38,7 @@ int iterative(int coin_index, int cost)
 		for (int j = 1; j <= cost; ++j)
 			if (j - coin[index] >= 0)
 				k[j] = min( k[j - coin[index]] +1 ,k[j] ) ;
+		//동전 1과 비슷.. goal을 만드는 coin의 개수는 goal - coin[i] 에 coin[i] 1개를 추가하는 것과 같다.
 	}
 
 	return k[cost];
