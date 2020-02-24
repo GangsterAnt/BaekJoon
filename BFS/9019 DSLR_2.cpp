faacc889261503d2b263900555c024d738213e14
GangsterAnt <49489178+GangsterAnt@users.noreply.github.com>
2020-02-24 21:05:16 +0900

    Create 9019 DSLR_2.cpp

---
+++ BFS/9019 DSLR_2.cpp
@@ -0,0 +1,143 @@
+//https://www.acmicpc.net/problem/9019
+//
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <map>
+
+using namespace std;
+
+vector<char> order;
+map< int, string > tosort;
+bool visited[10000];
+
+int rotateL(int in)
+{
+	int a, b, c, d;
+	a = in / 1000;
+	b = in / 100 - a * 10;
+	c = in / 10 - a * 100 - b * 10;
+	d = in % 10;
+
+
+	int out = b * 1000 + c * 100 + d * 10 + a;
+
+	return out;
+}
+int rotateR(int in)
+{
+	int a, b, c, d;
+	a = in / 1000;
+	b = in / 100 - a * 10;
+	c = in / 10 - a * 100 - b * 10;
+	d = in % 10;
+
+
+	int out = d * 1000 + a * 100 + b * 10 + c;;
+
+	return out;
+}
+
+void bfs(int start, int fin);
+
+int main()
+{
+	int c;
+	cin >> c;
+
+	while (c--)
+	{
+		int start, fin;
+		cin >> start >> fin;
+
+		tosort.clear();
+		memset(visited, false, sizeof(visited));
+
+		bfs(start, fin);
+		
+		/*cout << "Graph is Done\n";
+		map<int, char> ::iterator iter;
+
+		for (iter = tosort.begin(); iter != tosort.end(); iter++)
+		cout << "[" << iter->first << " , " << iter->second << " ]  \n";*/
+	
+		cout << tosort[fin] << '\n';
+	}
+
+	return 0;
+}
+
+
+void bfs(int start, int fin)
+{
+	
+
+	queue<int> q;
+	q.push(start);
+	visited[start] = true;
+
+	while (!q.empty())
+	{
+		const int here = q.front(); q.pop();
+		int temp;
+
+		if (here == fin)
+		{
+			return;
+		}
+		//D
+			temp = (here * 2 % 10000);
+
+			if (!visited[temp])
+			{
+				visited[temp] = true;
+				tosort[temp] = tosort[here] +  'D';
+				q.push(temp);
+
+			}
+
+			//S
+
+			//temp = (here - 1) < 0 ? 9999 : here - 1;
+			if (here == 0)
+				temp = 9999;
+
+			else
+				temp = here - 1;
+			if (!visited[temp])
+			{
+				visited[temp] = true;
+				tosort[temp] = tosort[here] + 'S';
+				q.push(temp);
+
+			
+
+			}
+
+			//L
+			temp = rotateL(here);
+			if (!visited[temp])
+			{
+				visited[temp] = true;
+				tosort[temp] = tosort[here] + 'L';
+				q.push(temp);
+
+			
+			}
+
+			//R
+			temp = rotateR(here);
+			if (!visited[temp])
+			{
+				visited[temp] = true;
+				tosort[temp] = tosort[here] + 'R';
+				q.push(temp);
+
+				
+			}
+		
+	}
+}
