82d3428adcba7568e41553237ffac3bddc1186a9
GangsterAnt <49489178+GangsterAnt@users.noreply.github.com>
2020-08-28 20:24:30 +0900

    Update 12757 전설의 JBNU.cpp

--- SW 역량 테스트 준비/12757 전설의 JBNU.cpp
+++ SW 역량 테스트 준비/12757 전설의 JBNU.cpp
@@ -1,5 +1,5 @@
 //https://www.acmicpc.net/problem/12757
-//timeout
+// 14528kb 240ms
 
 #include <iostream>
 #include <map>
@@ -9,31 +9,45 @@ using namespace std;
 
 int n, m, k;
 
-//정방향 map과 역방향 map2
+//mymap 은 key 가 정방향 
+// key 이상의 가장 작은 값을 찾는데 사용된다.
 map<int, int>mymap;
+
+//mymap2는 key가 * -1 되어있는 map
+// key 이하의 가장 큰 값을 찾는데 사용된다.
 map<int, int>mymap2;
 
-pair<int,int> nearestKey(int key)
+pair<int, int> nearestKey(int key)
 {
 	int plus = -1;
 	int minus = -1;
 
-//정방향 map 의 key 바로 다음 key를 찾아준다.
 	map<int, int>::iterator iter;
-	iter =mymap.lower_bound(key);
-  
-//존재시 업데이트
-	if( iter !=  mymap.end())
+	iter = mymap.lower_bound(key);
+
+	if (iter != mymap.end())
 		plus = iter->first;
 
+	//초기에는 iter-- 를 이용하였는데 iter가 mymap.begin() 이전으로 넘어가버리면 참조에러가 뜬다.
+	// iter > mymap.begin() 은 안되길래 걍 포기 역방향 map인 map2를 생성. 해결
 	iter = mymap2.lower_bound(-1 * key);
-//역방향 map 에서 key 바로 직전 key 를 찾아준다.
+
+	
 	if (iter != mymap2.end())
 		minus = (iter->first) *-1;
 
+	// K 보다 오차값이 크면 유효 X
+	if (plus - key > k)
+		plus = -1;
+
+	if (key - minus > k)
+		minus = -1;
+
+	// -1 이 리턴될경우 유효내에 키가 없다는뜻.
 	return make_pair(plus, minus);
 }
 
+// O( 2* K ) 느리다. 
 pair<int, int> nearestKey2(int key)
 {
 	int plus = -1;
@@ -85,7 +99,27 @@ void function2(int key, int value)
 		mymap[near.first] = value;
 		return;
 	}
-	
+
+	int p_diff = near.first - key;
+	int m_diff = key - near.second;
+
+	//동일거리 = 무시
+	if (p_diff == m_diff)
+	{
+		return;
+	}
+
+	if (m_diff > p_diff)
+	{
+		mymap[near.first] = value;
+	}
+	else
+	{
+		mymap[near.second] = value;
+	}
+
+
+
 }
 
 void function3(int key)
@@ -115,15 +149,30 @@ void function3(int key)
 
 	if (near.second == -1)
 	{
-		cout<<mymap[near.first] << '\n';
+		cout << mymap[near.first] << '\n';
 		return;
 	}
 	//근접키가 두개다
 
 
-	cout << "?\n";
-	return;
-	
+	int p_diff = near.first - key;
+	int m_diff = key - near.second;
+
+	//동일거리 
+	if (p_diff == m_diff)
+	{
+		cout << "?\n";
+		return;
+	}
+
+	if (m_diff > p_diff)
+	{
+		cout << mymap[near.first] << '\n';
+	}
+	else
+	{
+		cout << mymap[near.second] << '\n';
+	}
 
 
 }
