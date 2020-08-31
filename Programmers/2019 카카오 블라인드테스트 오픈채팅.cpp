2ceb27664f39fda55760612b930feaa643885d0e
GangsterAnt <49489178+GangsterAnt@users.noreply.github.com>
2020-08-31 16:55:14 +0900

    Update 2019 카카오 블라인드테스트 오픈채팅.cpp

--- Programmers/2019 카카오 블라인드테스트 오픈채팅.cpp
+++ Programmers/2019 카카오 블라인드테스트 오픈채팅.cpp
@@ -8,6 +8,9 @@
 
 using namespace std;
 
+//string s 는 Enter uid Nickname 이 ' '을 통해 구분되어있으므로 이를 통해 파싱해야한다.
+// int& i 는 명령어 "Enter ","Leave " (공백까지 6글자) "Change " ( " 7 글자) 를 넘어가기 위해서.
+// i 를 주소로 받음으로써 uid 를 파싱한후 Nickname 앞 index까지 i를 옮겨놓는다.
 string token_string(const string& s, int& i)
 {
 	string tmp = "";
