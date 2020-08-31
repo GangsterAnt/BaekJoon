d5e51bfb273bdbe7def1c787cdd2e23427b39a43
GangsterAnt <49489178+GangsterAnt@users.noreply.github.com>
2020-08-31 16:51:32 +0900

    Create 2019 카카오 블라인드테스트 오픈채팅.cpp

---
+++ Programmers/2019 카카오 블라인드테스트 오픈채팅.cpp
@@ -0,0 +1,70 @@
+//https://www.welcomekakao.com/learn/courses/30/lessons/42888?language=cpp
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+string token_string(const string& s, int& i)
+{
+	string tmp = "";
+	for (; i < s.size(); ++i)
+	{
+		if (s[i] == ' ')
+		{
+			i++; return tmp;
+		}
+		tmp += s[i];
+	}
+
+	return tmp;
+}
+
+vector<string> solution(vector<string> record)
+{
+	unordered_map<string, string>m;
+	vector<pair<char, string>>v;
+	vector<string> ans;
+
+	for (int i = 0; i < record.size(); ++i)
+	{
+		string uid = ""; string nickname = "";
+
+		if (record[i][0] == 'E')
+		{
+			int index = 6;
+			uid = token_string(record[i], index);
+			nickname = token_string(record[i], index);
+			v.emplace_back(make_pair('E', uid));
+			m[uid] = nickname;
+		}
+		else if (record[i][0] == 'L')
+		{
+			int index = 6;
+			uid = token_string(record[i], index);
+			v.emplace_back(make_pair('L', uid));
+		}
+		else
+		{
+			int index = 7;
+			uid = token_string(record[i], index);
+			nickname = token_string(record[i], index);
+			m[uid] = nickname;
+		}
+	}
+
+
+	for (int i = 0; i < v.size(); ++i)
+	{
+		string tmp = "";
+		if (v[i].first == 'E')
+			tmp = m[v[i].second] + "님이 들어왔습니다.";
+		else
+			tmp = m[v[i].second] + "님이 나갔습니다.";
+		ans.push_back(tmp);
+	}
+	return ans;
+}
