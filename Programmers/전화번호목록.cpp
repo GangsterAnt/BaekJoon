0805dfd90876c868dcf1dc76474ddfb7913719bd
GangsterAnt <49489178+GangsterAnt@users.noreply.github.com>
2020-09-08 01:41:45 +0900

    Create 전화번호목록.cpp

---
+++ Programmers/전화번호목록.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+	bool answer = true;
+	sort(phone_book.begin(), phone_book.end());
+
+	for (int i = 1; i < phone_book.size(); ++i)
+	{
+		string alpha = phone_book[i - 1];	
+		string beta = phone_book[i];
+
+		if (alpha.length() > beta.length())
+		{// beta 가 길게한다.
+			alpha = beta;
+			beta = phone_book[i - 1];
+		}
+
+		if (beta.find(alpha) == 0)
+		{
+			return false;
+		}
+		
+
+	}
+
+
+	return answer;
+}
