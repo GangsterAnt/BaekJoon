bb2bf0c847bc75e4f4b3f0c25e17b84f3d5399d4
GangsterAnt <49489178+GangsterAnt@users.noreply.github.com>
2020-09-08 18:37:00 +0900

    Update 위장.cpp

--- Programmers/위장.cpp
+++ Programmers/위장.cpp
@@ -31,7 +31,8 @@ int solution(vector<vector<string>> clothes) {
 	}
 
 	c_num = category.size();
-
+	// 모든 경우의수 => 옷의개수 + 안입을경우 해서 i 에 대해 총 m[category[i]]+1의 경우를 갖는다.
+	//하지만 모든 종류의 옷을 입지 않은경우는 제외해야하므로 ( 알몸인상태 ) 모두 안입은경우 1 을 빼준다.
 	for (int i = 0; i < c_num; ++i)
 	{
 		answer *= (m[category[i]]+1);
